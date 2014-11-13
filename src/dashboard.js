84c4f0a86cc034f09fe891abda708c4645de4411
Dmitry Bragin <dbragin@aboutecho.com>
2014-11-14 01:57:54 +0400

    Got rid of initial config, replaced endpoints by dependencies in app manifest

--- src/dashboard.js
+++ src/dashboard.js
@@ -143,26 +143,6 @@ dashboard.init = function() {
 	this.parent();
 };
 
-dashboard.methods.declareInitialConfig = function() {
-	return {
-		"targetURL": this.assembleTargetURL(),
-		"dependencies": {
-			"Janrain": {
-				"appId": this.getDefaultJanrainApp()
-			},
-			"StreamServer": {
-				"appkey": this.getDefaultAppKey()
-			},
-			"FilePicker": {
-				"apiKey": "AFLWUBllDRwWZl7sQO1V1z"
-			},
-			"embedly": {
-				"apiKey": "5945901611864679a8761b0fcaa56f87"
-			}
-		}
-	};
-};
-
 dashboard.dependencies = [{
 	"url": "{config:cdnBaseURL.apps.dataserver}/full.pack.js",
 	"control": "Echo.DataServer.Controls.Pack"
